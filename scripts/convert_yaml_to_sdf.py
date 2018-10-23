5572753e2a0d4543d1f0c9e8ded396039badf419
Dorus van Dinther <dorus_van_dinther@hotmail.com>
2018-10-23 22:53:21 +0200

    Cleaned script up a bit and updated

--- scripts/convert_yaml_to_sdf.py
+++ scripts/convert_yaml_to_sdf.py
@@ -22,10 +22,6 @@ file_type = "object"
 name = "couch"
 
 model_name = 'model name="{}"'.format(name)
-sdf_dict[sdf_dict.keys()[0]] = {model_name: {'link name="link"': {},
-                                             'pose': [0, 0, 0, 0, 0, 0],
-                                             'static': 'true'}}
-print sdf_dict
 # print sdf_dict[sdf_dict.keys()[0]][sdf_dict[sdf_dict.keys()[0]].keys()[0]]['link name="link"']
 
 with open("model.yaml", 'r') as stream:
@@ -35,6 +31,8 @@ with open("model.yaml", 'r') as stream:
     except yaml.YAMLError as exc:
         print(exc)
 print(x)
+
+# TEST CODE FOR EXPORTING TO XML, NOT WORKING CORRECTLY
 # test = {'sdf version="1.6"': {'model name = "couch"': {'pose': [0, 0, 0.5, 0, 0, 0], 'static': 'false'}}}
 #
 # xml = dicttoxml.dicttoxml(test)
@@ -43,36 +41,73 @@ print(x)
 
 # Filling the empty dictionary
 if file_type == "object":
+    # Is object, so create a (static) model type SDF dictionary (as opposed to world), with a default pose
+    sdf_dict[sdf_dict.keys()[0]] = {model_name: {'link name="link"': {},
+                                                 'pose': [0, 0, 0, 0, 0, 0],
+                                                 'static': 'true'}}
+    sdf_dict_link_level = sdf_dict[sdf_dict.keys()[0]][sdf_dict[sdf_dict.keys()[0]].keys()[0]]['link name="link"']
     if isinstance(x, dict):
-        counter = 0
+        shape_items_names = dict()
         for shape_items in x['shape']['compound']:
-            if shape_items.keys()[0] == "cylinder":
+            compound_type = shape_items.keys()[0]
+
+            # Check if compound type has name ('#' in yaml)
+            if '#' not in shape_items[compound_type]:
+                shape_items[compound_type]['#'] = compound_type
+
+            # Create names for different elements in the compound, making sure to avoid overwriting duplicate names
+            # by adding a counter for duplicate names. I.e. a name list of 'Leg', 'Leg' would become 'Leg', 'Leg02'
+            if 'collision name="{}"'.format(shape_items[compound_type]['#']) in sdf_dict_link_level:
+                shape_items_names[shape_items[compound_type]['#']] += 1
+                box_name = shape_items[compound_type]['#'] + "{:02d}".format(shape_items_names[shape_items\
+                    [compound_type]['#']])
+            else:
+                box_name = shape_items[compound_type]['#']
+                shape_items_names[shape_items[compound_type]['#']] = 1
+
+            shape_items_pose = [0, 0, 0, 0, 0, 0]
+            # Check if pose is in same dictionary level as compound type or not
+            if 'pose' in shape_items:
+                # Check if coordinates are in pose, if so overwrite default 0
+                if 'x' in shape_items['pose']:
+                    shape_items_pose[0] = shape_items['pose']['x']
+                elif 'y' in shape_items['pose']:
+                    shape_items_pose[1] = shape_items['pose']['y']
+                elif 'z' in shape_items['pose']:
+                    shape_items_pose[2] = shape_items['pose']['z']
+            elif 'pose' in shape_items[compound_type]:
+                # If 'pose' is in compound_type then check if coordinates are in that level and overwrite default 0
+                if 'x' in shape_items[compound_type]['pose']:
+                    shape_items_pose[0] = shape_items[compound_type]['pose']['x']
+                elif 'y' in shape_items[compound_type]['pose']:
+                    shape_items_pose[1] = shape_items[compound_type]['pose']['y']
+                elif 'z' in shape_items[compound_type]['pose']:
+                    shape_items_pose[2] = shape_items[compound_type]['pose']['z']
+
+            if compound_type == "cylinder":
+
+
+                # sdf_dict_link_level.update({'collision name="{}"'.format(box_name):
+                #                                 {'pose': shape_items_pose,
+                #                                  'geometry': {'cylinder': {'radius': ,
+                #                                                            'length': }}}})
+                # sdf_dict_link_level.update({'visual name="{} visual"'.format(box_name):
+                #                                 {'pose': shape_items_pose,
+                #                                  'geometry': {'cylinder': {'size': shape_items_size}}}})
+
                 print "Feature not yet implemented in this script"
-            elif shape_items.keys()[0] == "box":
-                sdf_dict[sdf_dict.keys()[0]][sdf_dict[sdf_dict.keys()[0]].keys()[0]]['link name="link"']\
-                    .update({'collision name="{}"'.format(shape_items['box']['#']):
-                                                 {'pose': [shape_items['box']['pose']['x'],
-                                                           shape_items['box']['pose']['y'],
-                                                           shape_items['box']['pose']['z'],
-                                                           0,
-                                                           0,
-                                                           0],
-                                                  'geometry': {'box': {'size': [shape_items['box']['size']['x'],
-                                                                                shape_items['box']['size']['y'],
-                                                                                shape_items['box']['size']['z']]}}}})
-                sdf_dict[sdf_dict.keys()[0]][sdf_dict[sdf_dict.keys()[0]].keys()[0]]['link name="link"'] \
-                    .update({'visual name="{} visual"'.format(shape_items['box']['#']):
-                                 {'pose': [shape_items['box']['pose']['x'],
-                                           shape_items['box']['pose']['y'],
-                                           shape_items['box']['pose']['z'],
-                                           0,
-                                           0,
-                                           0],
-                                  'geometry': {'box': {'size': [shape_items['box']['size']['x'],
-                                                                shape_items['box']['size']['y'],
-                                                                shape_items['box']['size']['z']]}}}})
-            counter = counter + 1
-            print shape_items
+            elif compound_type == "box":
+
+                shape_items_size = [shape_items['box']['size']['x'],
+                                    shape_items['box']['size']['y'],
+                                    shape_items['box']['size']['z']]
+
+                sdf_dict_link_level.update({'collision name="{}"'.format(box_name):
+                                                {'pose': shape_items_pose,
+                                                 'geometry': {'box': {'size': shape_items_size}}}})
+                sdf_dict_link_level.update({'visual name="{} visual"'.format(box_name):
+                                                {'pose': shape_items_pose,
+                                                 'geometry': {'box': {'size': shape_items_size}}}})
 
         # sdf_dict[sdf_dict.keys()[0]][sdf_dict[sdf_dict.keys()[0]].keys()[0]]
         for key in x:
@@ -84,4 +119,4 @@ if file_type == "object":
 elif file_type == "room":
     print "Dictionary item:", key
 
-print sdf_dict
+print "==============\nSDF Dictionary = " + str(sdf_dict)

e1ad3da9cd1d66fee35503270bc100a70ac67982
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-01-23 22:41:29 +0100

    prefix model paths, not appending

--- env-hooks/99.ed_object_models.bash
+++ env-hooks/99.ed_object_models.bash
@@ -8,16 +8,16 @@ function ed_object_models_setup {
     do
         if [[ "$dir" == "$model_path" ]]
         then
-            # You want to have the main model path at the end
+            # You want to have the main model path at the beginning
             continue
         fi
-        model_paths_string="${model_paths_string:+${model_paths_string}:}$dir"
+        model_paths_string="$dir${model_paths_string:+:${model_paths_string}}"
     done
 
-    model_paths_string="${model_paths_string:+${model_paths_string}:}$model_path"
+    model_paths_string="$model_path${model_paths_string:+:${model_paths_string}:}"
 
-    export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH:+${GAZEBO_MODEL_PATH}:}$model_paths_string
-    export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH:+${GAZEBO_RESOURCE_PATH}:}$model_paths_string
+    export GAZEBO_MODEL_PATH=$model_paths_string${GAZEBO_MODEL_PATH:+:${GAZEBO_MODEL_PATH}}
+    export GAZEBO_RESOURCE_PATH=$model_paths_string${GAZEBO_RESOURCE_PATH:+:${GAZEBO_RESOURCE_PATH}}
 }
 
 ed_object_models_setup

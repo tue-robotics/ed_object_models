d403ee0db9cdf67c31b3ffc720fae9e237e24ee6
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-01-24 16:20:45 +0100

    Update 99.ed_object_models.bash

--- env-hooks/99.ed_object_models.bash
+++ env-hooks/99.ed_object_models.bash
@@ -14,7 +14,7 @@ function ed_object_models_setup {
         model_paths_string="$dir${model_paths_string:+:${model_paths_string}}"
     done
 
-    model_paths_string="$model_path${model_paths_string:+:${model_paths_string}:}"
+    model_paths_string="$model_path${model_paths_string:+:${model_paths_string}}"
 
     export GAZEBO_MODEL_PATH=$model_paths_string${GAZEBO_MODEL_PATH:+:${GAZEBO_MODEL_PATH}}
     export GAZEBO_RESOURCE_PATH=$model_paths_string${GAZEBO_RESOURCE_PATH:+:${GAZEBO_RESOURCE_PATH}}

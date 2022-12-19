7da7a1b6f9e223065bd0f340332727f8cb201d39
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-12-19 15:02:02 +0100

    Update calc_rooms files

--- models/rwc2016a/calc_rooms.py
+++ models/rwc2016a/calc_rooms.py
@@ -1,7 +1,9 @@
+#! /usr/bin/env python
+
 import sys
 
 if len(sys.argv) <= 1:
-    print "Please provide room file"
+    print("Please provide room file")
     sys.exit(1)
 
 room_filename = sys.argv[1]
@@ -20,53 +22,56 @@ with open(room_filename) as f:
         if not room in data:
             data[room] = {}
 
-        data[room][args[1]] = (float(args[2].replace(",",".")), float(args[3].replace(",",".")))
+        data[room][args[1]] = (float(args[2].replace(",", ".")), float(args[3].replace(",", ".")))
 
 for (room, info) in data.iteritems():
 
     center = info["center"]
 
-    print """- type: "room"
-  id: "%s"
-  pose: { x: %.2f, y: %.2f, z: 0 }
+    print(
+        f"""- type: "room"
+  id: "{room}"
+  pose: { x: {center[0]:.2f}, y: {center[1]:.2f}, z: 0 }
   areas:
   - name: in
-    shape:""" % (room, center[0], center[1])
-    
+    shape:"""
+    )
 
     if "min1" in info:
 
         n = 1
         while True:
-            if not "min{}".format(n) in info:
+            if not f"min{n}" in info:
                 break
 
-            min = info["min{}".format(n)]
-            max = info["max{}".format(n)]
+            min = info[f"min{n}"]
+            max = info[f"max{n}"]
 
             rel_min = (min[0] - center[0], min[1] - center[1])
             rel_max = (max[0] - center[0], max[1] - center[1])
 
-
-            print """    - box:
-        min: { x: %.2f, y: %.2f, z: 0 }
-        max: { x: %.2f, y: %.2f, z: 0.01 }""" % (rel_min[0], rel_min[1], rel_max[0], rel_max[1])
+            print(
+                """    - box:
+        min: { x: {rel_min[0]:.2f}, y: {rel_min[1]:.2f}, z: 0 }
+        max: { x: {rel_max[0]:.2f}, y: {rel_max[1]:.2f}, z: 2.0 }"""
+            )
 
             n += 1
     else:
         n = 1
         points = []
         while True:
-            pname = "point{}".format(n)
+            pname = f"point{n}"
             if not pname in info:
                 break
             points.append(info[pname])
             n += 1
 
         if points:
-            print """    - polygon:
+            print(
+                """    - polygon:
         points:"""
+            )
 
             for p in points:
-                print "        - { x: %.2f, y: %.2f }" % (p[0], p[1])
-
+                print(f"        - { x: {p[0]:.2f}, y: {p[1]:.2f} }")
